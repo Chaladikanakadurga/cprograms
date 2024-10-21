688ec8d27357301a4bccfbf34bfe55d1eca79471
Chaladikanakadurga <chaladikanakadurga@gmail.com>
2024-10-21 12:31:58 +0530

    Create vowelornot.c

---
+++ vowelornot.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter character");
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+    printf("character is vowel");\
+}
+else{
+    printf("character is not vowel");
+}
+}
